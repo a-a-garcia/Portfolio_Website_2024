651b327133aa08f1b28f6fcd8c58ae751e4463c0
Anthony <angarce01@gmail.com>
2024-01-28 17:28:41 -0800

    animations fix on /contact and /about-me

--- src/Components/customTransition.js
+++ src/Components/customTransition.js
@@ -1,14 +1,19 @@
 import { useEffect } from "react";
 
 const useCustomTransition = (selector, classes) => {
-  useEffect(() => {
-    const element = document.querySelector(selector);
-    element.classList.add(...classes);
-
-    return () => {
-      element.classList.remove(...classes);
-    };
-  }, [selector, classes]);
-};
+    useEffect(() => {
+      const addClasses = () => {
+        const element = document.querySelector(selector);
+        if (element) {
+          element.classList.add(...classes);
+          return () => {
+            element.classList.remove(...classes);
+          };
+        }
+      };
+      const timeoutId = setTimeout(addClasses, 0);
+      return () => clearTimeout(timeoutId);
+    }, [selector, classes]);
+  };
 
 export default useCustomTransition;
