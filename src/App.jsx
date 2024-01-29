de74fa57b72f3b8bad6591eb37984b764616dbb2
Anthony <angarce01@gmail.com>
2024-01-28 17:07:31 -0800

    routes debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Nav from "./Components/Nav";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingText from "./Components/LandingText";
 import ProjectsPage from "./Components/ProjectsPage";
 import AboutMePage from "./Components/AboutMePage";
@@ -9,7 +9,7 @@ import ContactMe from "./Components/ContactMe";
 function App() {
   return (
     <div className="page-container mx-10 max-h-100">
-      <BrowserRouter>
+      <HashRouter>
         <Nav />
         <Routes>
           <Route path="/" element={<LandingText />}></Route>
@@ -17,7 +17,7 @@ function App() {
           <Route path="/about-me" element={<AboutMePage/>}></Route>
           <Route path="/contact" element={<ContactMe />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
